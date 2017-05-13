a7f83517d29e0f41a96aa36c2c966404bb9d629b
404wasfound <bgd.yur@gmail.com>
2017-05-13 16:27:10 +0200

    Added assets for OnboardingPernissions VC, created xib file.

---
+++ Donly.de/OnboardingPermissionsViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingPermissionsViewModel.swift
+//  Donly.de
+//
+//  Created by Bogdan Yur on 5/13/17.
+//  Copyright © 2017 404wasfound. All rights reserved.
+//
+
+import Foundation
+
+protocol OnboardingPermissionsViewModelProtocol {
+  /// dunno
+}
+
+class OnboardingPermissionsViewModel: OnboardingPermissionsViewModelProtocol {
+  
+  init() {
+    ///
+  }
+  
+}
