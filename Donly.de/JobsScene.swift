3ed72b55734cdefc02efbbe5ad5c7b5f06597545
404wasfound <bgd.yur@gmail.com>
2017-06-17 17:38:50 +0200

    Added assets / classes for the Jobs views.

---
+++ Donly.de/JobsScene.swift
@@ -0,0 +1,9 @@
+//
+//  JobsScene.swift
+//  Donly.de
+//
+//  Created by Bogdan Yur on 6/17/17.
+//  Copyright © 2017 404wasfound. All rights reserved.
+//
+
+import Foundation
---
+++ Donly.de/JobsVC.swift
@@ -0,0 +1,19 @@
+//
+//  JobsVC.swift
+//  Donly.de
+//
+//  Created by Bogdan Yur on 6/17/17.
+//  Copyright © 2017 404wasfound. All rights reserved.
+//
+
+import UIKit
+
+class JobsVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+
+}
